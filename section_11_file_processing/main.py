989988b1383d27eb0881675a0d0255478a910838
iiPaysije <pavleljubenovic99@gmail.com>
2024-03-20 15:26:13 +0100

    completed section 11

--- section_11_file_processing/main.py
+++ section_11_file_processing/main.py
@@ -1,8 +1,21 @@
 import os
 
-# Exmplanation for this fix 
+# Exmplanation for this fix
 # https://stackoverflow.com/questions/4060221/how-to-reliably-open-a-file-in-the-same-directory-as-the-currently-running-scrip
+
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-f = open(os.path.join(__location__, "fruits.txt"))
-print(f.read())
+with open(os.path.join(__location__, "vegetables.txt"), "w") as f:
+    content = f.read()
+
+f.close()
+
+print(content)
+
+# Append and read the content of the file
+
+with open('file.txt', '+a') as file:
+    content = file.write("Some new randon text")
+    file.seek(0)
+    content = file.read()
+    print(content)
