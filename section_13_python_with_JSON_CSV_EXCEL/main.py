d180bf93ce57adf1b7efb8ed82c7c2e6657aaf3b
iiPaysije <pavleljubenovic99@gmail.com>
2024-04-03 17:26:43 +0200

    created Jupyter notebook, practiced to use ArcGis for extracting latitude and longitude from table rows, and inserted new colums for latitude and longitude

--- section_13_python_with_JSON_CSV_EXCEL/main.py
+++ section_13_python_with_JSON_CSV_EXCEL/main.py
@@ -1,5 +1,6 @@
 import os
 import pandas
+from geopy.geocoders import ArcGIS
 
 # Exmplanation for this fix
 # https://stackoverflow.com/questions/4060221/how-to-reliably-open-a-file-in-the-same-directory-as-the-currently-running-scrip
@@ -37,12 +38,55 @@ dataFrameComma = pandas.read_csv(
 )
 
 # Setting a custom header row with name of each cell
-dataFrameComma.columns = ["ID", "Address", "City", "ZIP", "Country", "Name", "Employees"]
+dataFrameComma.columns = [
+    "ID",
+    "Address",
+    "City",
+    "ZIP",
+    "Country",
+    "Name",
+    "Employees",
+]
 
-# Setting a custom index 
-dataFrameComma.set_index("ID", drop= False, inplace= True)
+# Setting a custom index
+# dataFrameComma.set_index("ID", drop=False, inplace=True)
 
 # Locate specific data from DataFrame
-print(dataFrameComma.loc["2":"4", "Name"] , "\n\n")
+# print(dataFrameComma.loc["2":"4", "Name"] , "\n\n")
 
+# Add another column to the table
+# dataFrameComma["Continent"] = dataFrameComma.shape[0]*["North Amerrica"]
+
+# new column Continent is made from Column Country adn 2 more strings
+dataFrameComma["Continent"] = dataFrameComma["Country"] + "," + "North America"
+
+# Add another Row - first make new variable, then transpose original table , then add , then transpose again
+
+dataFrameComma_t = dataFrameComma.T
+dataFrameComma_t["6"] = [
+    "7",
+    "My Adress",
+    "My City",
+    "My ZIP",
+    "My Country",
+    "My Name",
+    "0",
+    "My Continent",
+]
+dataFrameComma = dataFrameComma_t.T
+
+# Combining addresses and ZIP codes
+dataFrameComma["Address"] = dataFrameComma["Address"] + "," + dataFrameComma["City"] + "," + dataFrameComma["ZIP"] 
+
+print(dataFrameComma , "\n\n")
+
+# Working with geocodig
+nom = ArcGIS()
+
+# Create new column, add all coordinates from every address using geocoder
+dataFrameComma["Coordinates"] = dataFrameComma["Address"].apply(nom.geocode)
+
+# Extract latitude and longitude and add to separaate columns
+dataFrameComma["Latitude"] = dataFrameComma["Coordinates"].apply(lambda x: x.latitude if x != None else None)
+dataFrameComma["Longitude"] = dataFrameComma["Coordinates"].apply(lambda x: x.longitude if x != None else None)
 print(dataFrameComma)
